f278125b91f1ac2092f9a858214b8dcca9dde5f2
lipsanen <jukspaz@gmail.com>
2026-04-27 00:35:29 +0300

    Add lua functions for console commands and tick/frame hooks

--- spt/features/lua.cpp
+++ spt/features/lua.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.hpp"
 #include "..\feature.hpp"
 #include "file.hpp"
+#include "sptlib-wrapper.hpp"
+#include "signals.hpp"
 
 
 #define lua_c
@@ -69,6 +71,139 @@ static int docall(lua_State* L, int narg, int nres) {
 	return status;
 }
 
+
+enum luacallbacks {
+    on_tick,
+    on_frame,
+    num_lua_callbacks
+};
+
+static const char* callback_func_names[num_lua_callbacks] = {};
+
+static int callback_funs[num_lua_callbacks];
+
+static void init_lua_callbacks()
+{
+	for (size_t i = 0; i < num_lua_callbacks; ++i)
+	{
+		callback_funs[i] = LUA_NOREF;
+	}
+
+	if (TickSignal.Works)
+	{ 
+		callback_func_names[on_tick] = "on_tick";
+	}
+
+	if (FrameSignal.Works)
+	{
+		callback_func_names[on_frame] = "on_frame";
+	}
+}
+
+static enum luacallbacks get_callback(lua_State* L, const char* string)
+{
+    for (size_t i = 0; i < num_lua_callbacks; ++i)
+    {
+        if (strcmp(string, callback_func_names[i]) == 0)
+        {
+            return (enum luacallbacks)i;
+        }
+    }
+
+    luaL_error(L, "callback %s doesn't exist", string);
+    return num_lua_callbacks;
+}
+
+static int register_hook(lua_State* L) {
+    luaL_checktype(L, 1, LUA_TFUNCTION);
+    size_t len;
+    const char* func_name = luaL_checklstring(L, 2, &len);
+
+    enum luacallbacks cb = get_callback(L, func_name);
+
+    if (callback_funs[(size_t)cb] != LUA_NOREF) {
+        luaL_unref(L, LUA_REGISTRYINDEX, callback_funs[(size_t)cb]);
+    }
+
+    /* Push the function to the top of the stack (it's already at index 1,
+        but luaL_ref pops from the top) */
+    lua_pushvalue(L, 1);
+
+    /* Store it in the registry and save the reference */
+    callback_funs[(size_t)cb] = luaL_ref(L, LUA_REGISTRYINDEX);
+
+    return 0;
+}
+
+static int lua_concommand(lua_State* L) {
+	size_t len;
+    const char* command = luaL_checklstring(L, 1, &len);
+	EngineConCmd(command);
+
+    return 0;
+}
+
+static void fire_callback(lua_State* L, enum luacallbacks cb)
+{
+    if (callback_funs[(size_t)cb] == LUA_NOREF) {
+        return; /* no callback registered */
+    }
+
+    /* Push the function from the registry onto the stack */
+    lua_rawgeti(L, LUA_REGISTRYINDEX, callback_funs[(size_t)cb]);
+
+    /* Call: 1 argument, 0 return values, no error handler */
+    if (lua_pcall(L, 0, 0, 0) != LUA_OK) {
+        fprintf(stderr, "Lua callback error: %s\n", lua_tostring(L, -1));
+        lua_pop(L, 1); /* pop error message */
+    }
+}
+
+static const luaL_Reg lua_spt_lib[] = {
+    {"hook", register_hook},
+	{"console", lua_concommand},
+    {NULL, NULL}
+};
+
+LUAMOD_API int luaopen_spt(lua_State* L) {
+    luaL_newlib(L, lua_spt_lib);
+	init_lua_callbacks();
+
+    return 1;
+}
+
+/*
+** Check whether 'status' is not OK and, if so, prints the error
+** message on the top of the stack.
+*/
+static int report(lua_State* L, int status) {
+	if (status != LUA_OK) {
+		const char* msg = lua_tostring(L, -1);
+		if (msg == NULL)
+			msg = "(error message not a string)";
+		Warning("lua error: %s\n", msg);
+		lua_pop(L, 1);  /* remove message */
+	}
+	return status;
+}
+
+
+static int dochunk(lua_State* L, int status) {
+	if (status == LUA_OK) status = docall(L, 0, 0);
+	return report(L, status);
+}
+
+
+static int dofile(lua_State* L, const char* name) {
+	return dochunk(L, luaL_loadfile(L, name));
+}
+
+
+static int dostring(lua_State* L, const char* s, const char* name) {
+	return dochunk(L, luaL_loadbuffer(L, s, strlen(s), name));
+}
+
+
 CON_COMMAND_AUTOCOMPLETEFILE(spt_lua_script, "Run .lua script", FCVAR_CHEAT, "lua", ".lua")
 {
 	if (args.ArgC() == 1)
@@ -78,17 +213,38 @@ CON_COMMAND_AUTOCOMPLETEFILE(spt_lua_script, "Run .lua script", FCVAR_CHEAT, "lu
 	}
 
 	std::string path = GetGameDir() + "/lua/" + args.Arg(1) + ".lua";
-	int status = luaL_loadfile(lua_feature.L, path.c_str());
+	dofile(lua_feature.L, path.c_str());
+}
 
-	if (status != LUA_OK)
+CON_COMMAND_F(spt_lua_run, "Run inline script", FCVAR_CHEAT)
+{
+	if (args.ArgC() == 1)
 	{
-		Warning("Running script %s failed with error code %d\n", args.Arg(1), status);
+		Msg("Usage: spt_lua_run <inline script>\n");
 		return;
 	}
 
-	docall(lua_feature.L, 0, 0);
+#ifndef OE
+	dostring(lua_feature.L, args.ArgS(), "");
+#else
+	dostring(lua_feature.L, args.Arg(1), "");
+#endif
 }
 
+static void init_spt_lua()
+{
+	lua_feature.L = luaL_newstate();
+	luaL_openselectedlibs(lua_feature.L, ~0, 0);
+	luaL_requiref(lua_feature.L, "spt", luaopen_spt, 1);
+	lua_gc(lua_feature.L, LUA_GCRESTART);  /* start GC... */
+	lua_gc(lua_feature.L, LUA_GCGEN);  /* ...in generational mode */
+}
+
+CON_COMMAND_F(spt_lua_reset, "Reset lua state", FCVAR_CHEAT)
+{
+	lua_close(lua_feature.L);
+	init_spt_lua();
+}
 
 bool LuaFeature::ShouldLoadFeature()
 {
@@ -97,13 +253,20 @@ bool LuaFeature::ShouldLoadFeature()
 
 void LuaFeature::InitHooks() {}
 
+static void on_frame_hook() { fire_callback(lua_feature.L, on_frame);  }
+static void on_tick_hook(bool simulating) { fire_callback(lua_feature.L, on_tick); }
+
+
 void LuaFeature::LoadFeature() 
 {
 	InitCommand(spt_lua_script);
-	L = luaL_newstate();
-	luaL_openselectedlibs(L, ~0, 0);
-	lua_gc(L, LUA_GCRESTART);  /* start GC... */
-	lua_gc(L, LUA_GCGEN);  /* ...in generational mode */
+	InitCommand(spt_lua_run);
+	InitCommand(spt_lua_reset);
+
+	init_spt_lua();
+
+	FrameSignal.Connect(on_frame_hook);
+	TickSignal.Connect(on_tick_hook);
 }
 
 void LuaFeature::UnloadFeature() 
